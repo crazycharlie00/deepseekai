feb605fc8481b03dd919637e6c817ceabfa05905
crazycharlie00 <crazycharlie1998@gmail.com>
2025-01-30 21:05:00 +0100

    first look

---
+++ script.js
@@ -0,0 +1,4 @@
+function connectToAgent(agentName) {
+    alert(`Connecting to ${agentName} via the DeepSeek API...`);
+    // Here you can add the actual API call to connect to the DeepSeek API.
+  }
